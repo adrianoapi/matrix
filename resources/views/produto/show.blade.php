78f6211eade376276232946bb7e01928dd2ed662
Adriano API <adrianoapi@hotmail.com>
2019-01-26 15:45:10 -0200

    update show page

--- resources/views/produto/show.blade.php
+++ resources/views/produto/show.blade.php
@@ -1,12 +1,24 @@
 @extends('layout.app')
-@section('title', 'Listagem de produtos')
+@section('title', $produto->titulo)
 @section('content')
-    <h1>{{$produto->titulo}}</h1>
-    <ul>
+<div class="container">
+    <h1>Produto {{$produto->titulo}}</h1>
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+      <ul>
         <li>Referência: {{$produto->referencia}}</li>
-        <li>Preço: {{$produto->preco}}</li>
-        <li>Adicionado em: {{$produto->created_at}}</li>
-    </ul>
-    <p>{{$produto->descricao}}</p>
+        <li>Preço: R${{ number_format($produto->preco,2,',','.')}}</li>
+        <li>Adicionado em: {{ date("d/m/Y",strtotime($produto->created_at)) }}</li>
+      </ul>
+      <p>{{$produto->descricao}}</p>
+      </div>
+      @if(file_exists("./img/produtos/" . md5($produto->id) . ".jpg"))
+        <div class="col-xs-6 col-md-3">
+            <a href="{{asset("img/produtos/" . md5($produto->id) . ".jpg")}}" class="thumbnail" style="width: 200px;">
+            {{Html::image(asset("img/produtos/" . md5($produto->id) . ".jpg"), $produto->titulo, array( 'width' => 70, 'height' => 70 ))}}
+          </a>
+        </div>
+      @endif
+    </div>
     <a href="javascript:history.go(-1)">Voltar</a>
 @endsection
