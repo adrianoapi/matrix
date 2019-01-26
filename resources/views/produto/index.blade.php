f535d251232f8bbc650c60c8b083423f8c580b47
Adriano API <adrianoapi@hotmail.com>
2019-01-26 12:20:41 -0200

    add blade

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -1,15 +1,12 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Produtos</title>
-    </head>
-    <body>
-        <ul>
-            @foreach ($produtos as $produto)
-                <li>
-                    <a href="/produtos/{{$produto->id}}">{{$produto->titulo}}</a>
-                </li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@extends('layout.app')
+@section('title', 'Listagem de produtos')
+@section('content')
+    <h1>Produtos</h1>
+    <ul>
+        @foreach ($produtos as $produto)
+            <li>
+                <a href="/produtos/{{$produto->id}}">{{$produto->titulo}}</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
--- resources/views/produto/show.blade.php
+++ resources/views/produto/show.blade.php
@@ -1,16 +1,12 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Produto {{$produto->titulo}}</title>
-    </head>
-    <body>
-        <h1>{{$produto->titulo}}</h1>
-        <ul>
-            <li>Referência: {{$produto->referencia}}</li>
-            <li>Preço: {{$produto->preco}}</li>
-            <li>Adicionado em: {{$produto->created_at}}</li>
-        </ul>
-        <p>{{$produto->descricao}}</p>
-        <a href="javascript:history.go(-1)">Voltar</a>
-    </body>
-</html>
+@extends('layout.app')
+@section('title', 'Listagem de produtos')
+@section('content')
+    <h1>{{$produto->titulo}}</h1>
+    <ul>
+        <li>Referência: {{$produto->referencia}}</li>
+        <li>Preço: {{$produto->preco}}</li>
+        <li>Adicionado em: {{$produto->created_at}}</li>
+    </ul>
+    <p>{{$produto->descricao}}</p>
+    <a href="javascript:history.go(-1)">Voltar</a>
+@endsection
