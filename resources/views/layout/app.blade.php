9873b4f69be14e636ce420558d7d7f863cf04a9b
Adriano API <adrianoapi@hotmail.com>
2019-01-26 12:42:59 -0200

    add template

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,10 +2,14 @@
 <html>
     <head>
         <title>Matrix - @yield('title')</title>
+        {{Html::style('css/bootstrap.min.css')}}
+        {{Html::style('css/bootstrap-theme.min.css')}}
     </head>
     <body>
         <div class="container">
             @yield('content')
         </div>
+        {{Html::script('js/jquery-3.3.1.min.js')}}
+        {{Html::script('js/bootstrap.min.js')}}
     </body>
 </html>
